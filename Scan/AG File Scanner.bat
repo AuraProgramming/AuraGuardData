14d9ddbcf78c910b89962d3c292bf4e4c8381a8d
AuraClient <82337274+AuraClient@users.noreply.github.com>
2021-05-03 15:01:48 -0400

    Update AG File Scanner.bat

--- Scan/AG File Scanner.bat
+++ Scan/AG File Scanner.bat
@@ -5,7 +5,7 @@ set /p file=
 powershell Get-FileHash %file% -Algorithm MD5 ^| Format-Table -Property Hash>scannedhash.txt
 cls
 for /f "tokens=* delims= " %%x in (scannedhash.txt) do (
-	for /f "tokens=* delims= " %%y in (AG DB.txt) do (
+	for /f "tokens=* delims= " %%y in ("AG DB.txt") do (
 		if "%%x" == "%%y" (
 			ren "%file%" "%file%.AGQ"
 			move "%file%" "C:\Program Files\Aura Guard\AG Quarantine"
