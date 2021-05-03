0ead538e291ad694683cfd94d8fdc206f8529a9a
AuraClient <82337274+AuraClient@users.noreply.github.com>
2021-05-03 15:41:45 -0400

    Update AG Setup.bat

--- AG Setup.bat
+++ AG Setup.bat
@@ -1,4 +1,10 @@
 @echo off
-mkdr "\AuraGuardSetupTemp"
-mkdr "\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard"
-CD %USERPROFILE%\AppData\Local\Temp\
+mkdr "C:\Extract AG Here\"
+echo Extract the file to C:\Extract AG Here\ and rename the folder to AuraGuard
+mkdr "C:\AuraGuard\"
+mkdr "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+move "C:\Extract AG Here\AuraGuard\AuraGuard-Basic\Scan\AG File Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+move "C:\Extract AG Here\AuraGuard\AuraGuard-Basic\Scan\AG Folder Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+move "C:\Extract AG Here\AuraGuard\AuraGuard-Basic\Scan\AG Folder Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+move "C:\Extract AG Here\AuraGuard\AuraGuard-Basic\Cleanup\AG Cleanup.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+move "C:\Extract AG Here\AuraGuard\AuraGuard-Basic\Cleanup\AG Temp Scrub.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
