c9615e72cf01b1983261ede8b560ada1b559ae24
AuraClient <82337274+AuraClient@users.noreply.github.com>
2021-05-03 14:59:32 -0400

    Create AG Website Scanner.bat

---
+++ Scan/AG Website Scanner.bat
@@ -0,0 +1,19 @@
+@echo off
+title AG URL Scanner
+echo Enter URL:
+set /p url= 
+echo "ping %url%">>url.txt
+cls
+for /f "tokens=* delims= " %%x in (url.txt) do (
+	for /f "tokens=* delims= " %%y in (AG DB IP.txt) do (
+		if "%%x" == "%%y" (
+      taskkill /IM chrome.exe /T /F
+			cls
+		)
+	)
+)
+del /q url.txt
+echo. No threats found!
+pause
+cls
+exit
