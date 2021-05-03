45ae50b53ee1d70cd55eafc95e983155a7093c53
AuraClient <82337274+AuraClient@users.noreply.github.com>
2021-05-03 15:44:42 -0400

    Fixed Typos :o

--- AG Setup.bat
+++ AG Setup.bat
@@ -1,9 +1,9 @@
 @echo off
-mkdr "C:\Extract AG Here\"
+mkdir "C:\Extract AG Here\"
 echo Extract the file to C:\Extract AG Here\ and rename the folder to AuraGuard
 pause
-mkdr "C:\AuraGuard\"
-mkdr "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+mkdir "C:\AuraGuard\"
+mkdir "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
 move "C:\Extract AG Here\AuraGuard\AuraGuard-Basic\Scan\AG File Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
 move "C:\Extract AG Here\AuraGuard\AuraGuard-Basic\Scan\AG Folder Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
 move "C:\Extract AG Here\AuraGuard\AuraGuard-Basic\Scan\AG Folder Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
