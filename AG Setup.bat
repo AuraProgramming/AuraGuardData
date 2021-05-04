6e4df4465b9a49e62e71a00cdaa829e7bf14cced
AuraClient <82337274+AuraClient@users.noreply.github.com>
2021-05-04 10:38:12 -0400

    Update AG Setup.bat

--- AG Setup.bat
+++ AG Setup.bat
@@ -4,8 +4,8 @@ echo Extract the file to C:\AuraGuard\ and rename the folder to AuraGuard
 pause
 mkdir "C:\AuraGuard\"
 mkdir "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
-move "C:\AuraGuard\AuraGuard\AuraGuard-Basic\Scan\AG File Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
-move "C:\AuraGuard\AuraGuard\AuraGuard-Basic\Scan\AG Folder Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
-move "C:\AuraGuard\AuraGuard\AuraGuard-Basic\Scan\AG Folder Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
-move "C:\AuraGuard\AuraGuard\AuraGuard-Basic\Cleanup\AG Cleanup.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
-move "C:\AuraGuard\AuraGuard\AuraGuard-Basic\Cleanup\AG Temp Scrub.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+move "C:\AuraGuard\AuraGuard\AuraGuardOS-Basic\Scan\AG File Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+move "C:\AuraGuard\AuraGuard\AuraGuardOS-Basic\Scan\AG Folder Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+move "C:\AuraGuard\AuraGuard\AuraGuardOS-Basic\Scan\AG Folder Scanner.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+move "C:\AuraGuard\AuraGuard\AuraGuardOS-Basic\Cleanup\AG Cleanup.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
+move "C:\AuraGuard\AuraGuard\AuraGuardOS-Basic\Cleanup\AG Temp Scrub.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\AuraGuard\"
