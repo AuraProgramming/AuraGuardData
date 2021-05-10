0d7ae1ed51da12feeb62e22fd8ee306e8647becf
AuraProgramming <83914971+AuraProgramming@users.noreply.github.com>
2021-05-10 17:17:05 -0400

    Delete Setup.bat

--- Setup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Please Extract AuraGuardOS-BETA0.5.zip to your downloads folder
-pause
-ren "C:\Users\%USERFROFILE%\Downloads\AuraGuardOS-BETA0.5" "AuraGuard"
-ren "C:\Users\%USERFROFILE%\Downloads\AuraGuardOS-BETA0.5\AuraGuardOS-BETA0.5" "AuraGuardOS"
-move "C:\Users\%USERPROFILE%\Downloads\AuraGuard" "C:\"
