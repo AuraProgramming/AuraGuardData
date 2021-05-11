7bee378c04b51f460c69bc6230272dd1d32764b0
AuraProgramming <83914971+AuraProgramming@users.noreply.github.com>
2021-05-11 09:07:31 -0400

    Update Cleanup.bat

--- Cleanup/Cleanup.bat
+++ Cleanup/Cleanup.bat
@@ -10,35 +10,38 @@
       IF NOT EXIST %folder% goto top
     
 :cont
-  echo Are you sure you would like to clean %folder% of executables? Y/N
-    choice /c yn /n
-      if errorlevel 2 goto N
-      if errorlevel 1 goto Y
+  :confirm:
+    echo Are you sure you would like to clean %folder% of executables? Y/N
+      choice /c yn /n
+        if errorlevel 2 goto N
+        if errorlevel 1 goto Y
 
 :Y
-  echo Cleaning up...
-    @echo on
-      del /q %folder%\*.exe
-      del /q %folder%\*.bat
-      del /q %folder%\*.com
-      del /q %folder%\*.vbs
-      del /q %folder%\*.js
-      del /q %folder%\*.py
-      del /q %folder%\*.cmd
-      del /q %folder%\*.apk
-      del /q %folder%\*.run
-      del /q %folder%\*.jar
-      del /q %folder%\*.bin
-      del /q %folder%\*.app
-      del /q %folder%\*.e_e
-        @echo off
-          echo Process has been completed!
-            pause
-              cls
+  :delete all executables:
+    echo Cleaning up...
+      @echo on
+        del /q %folder%\*.exe
+        del /q %folder%\*.bat
+        del /q %folder%\*.com
+        del /q %folder%\*.vbs
+        del /q %folder%\*.js
+        del /q %folder%\*.py
+        del /q %folder%\*.cmd
+        del /q %folder%\*.apk
+        del /q %folder%\*.run
+        del /q %folder%\*.jar
+        del /q %folder%\*.bin
+        del /q %folder%\*.app
+        del /q %folder%\*.e_e
+          @echo off
+            echo Process has been completed!
+              pause
+                cls
                 exit
                
 :N
-  echo Canceling
-    pause
-      cls
-      exit
+  :exit:
+    echo Canceling
+      pause
+        cls
+        exit
